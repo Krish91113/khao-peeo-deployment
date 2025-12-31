6e4b586de400ee4e1e05b95d86ab92683a3d3607
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-31 13:10:34 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ import app from "./app.js";
 import { connectDB } from "./config/db.js";
 import { ENV } from "./config/env.js";
 
+
 const server = http.createServer(app);
 
 const start = async () => {
   await connectDB();
 
+
   server.listen(ENV.PORT, () => {
+    console.log(`Server is running on port http://localhost:${ENV.PORT}`)
   });
 };
 
